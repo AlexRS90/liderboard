3c07c6f0c8b61134db84f9ad21c29b23e56293cb
AlexRS90 <aramosbengy@gmail.com>
2021-11-09 15:55:03 -0600

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+    const element = document.createElement('div');
+  
+    // Lodash, 
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+  
+    return element;
+  }
+  
+  document.body.appendChild(component());
