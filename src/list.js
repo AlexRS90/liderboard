56e0db982f72b7d8dbb956d4d8cb73b3657c44de
AlexRS90 <aramosbengy@gmail.com>
2021-11-10 22:13:41 -0600

    Add localstorage & form eventListener

--- src/list.js
+++ src/list.js
@@ -1,3 +1,5 @@
+import * as bowling from "./api";
+
 const bestScores = [
   {
     name: 'Alejandro',
@@ -17,12 +19,33 @@ const bestScores = [
   },
 ];
 
+let idMatch = '';
+
+const playGame = () => {
+  if(window.localStorage.getItem('bowlingMatch')) {
+    idMatch = window.localStorage.getItem('bowlingMatch');
+  } else {
+    bowling.callApi();
+    idMatch = window.localStorage.getItem('bowlingMatch');
+  }
+};
+
 const fillList = () => {
   let newWinner = '';
   bestScores.forEach((winner) => {
     newWinner += `<li class="d-flex top-scores"><p>${winner.name}</p><p>${winner.score}</p></li>`;
   });
   document.querySelector('.best-scores').innerHTML = newWinner;
+  playGame();
 };
+
+const submitBtn = document.querySelector('#send-new-score');
+submitBtn.addEventListener('click', (event) => {
+  const newPlayer = document.getElementById('new-player').value;
+  const newScore = document.getElementById('new-score').value;
+  if (newPlayer !== '' && newScore !== '') {
+    bowling.newScores(newPlayer, newScore, idMatch);
+  }
+});
 
 export default fillList;
